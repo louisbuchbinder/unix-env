0e3e80c024efc888a54a46a96cc0183fbe537be9
Louis Buchbinder <git@louisbuchbinder.com>
2020-06-11 17:18:34 -0700

    updated the run-ctags.py script

--- .bin/run-ctags.py
+++ .bin/run-ctags.py
@@ -6,6 +6,32 @@ tagfile = '.git/tags'
 tagfile_prefix = tagfile + '_' + dedupe
 tagfile_temp = tagfile_prefix + '_' + timestamp
 
-subprocess.run(['ctags', '-f', tagfile_temp, '.'], stderr=subprocess.DEVNULL)
-subprocess.run(['mv', tagfile_temp, tagfile])
+ps = subprocess.Popen(
+  [
+    'ps',
+    '-ax',
+    '-o',
+    'pid,command'
+  ],
+  stdout=subprocess.PIPE
+)
+
+running = []
+count = -1
+while True:
+  line = ps.stdout.readline()
+  count += 1
+  if count == 0: continue
+  if not line: break
+  pid, *command = line.decode().strip().split(' ')
+  if tagfile_prefix in ' '.join(command): running.append([pid, command])
+
+for [pid, command] in running:
+  tmpfile = command[2]
+  subprocess.run(["kill", pid])
+  os.remove(tmpfile)
+
+ctags = subprocess.run(['ctags', '-f', tagfile_temp, '.'], stderr=subprocess.DEVNULL)
+if ctags.returncode == 0:
+  subprocess.run(['mv', tagfile_temp, tagfile])
 
